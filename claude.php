1e503a1bbdd2b40c1e84fec88ae29979d72431df
rdmnlee <rodmanperezlee@gmail.com>
2021-03-11 16:50:10 +0800

    Revert "removed other about page from feature/rodman"
    This reverts commit 9d5101b8d5788b176dd01e5eb0c4f76726622982.

---
+++ claude.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+        <title>Album example for Bootstrap</title>
+
+        <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/album/">
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+        <!-- Custom styles for this template -->
+        <link href="album.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Hi I'm Claude</h1>
+            <p>This is my about me page. I will add more content later.</p>
+            <a href="/">Back Home</a>
+        </div>
+    </body>
+</html>
