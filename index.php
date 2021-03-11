4a96b7e26bdfbaf6b7a47382c04a961df8139c58
Michael <michaeldimakilingece@gmail.com>
2021-03-11 23:18:23 +0800

    Michael.php

--- index.php
+++ index.php
@@ -109,7 +109,20 @@
                                 </div>
                             </div>
                         </div>
-
+                        <div class="col-md-4">
+                            <div class="card mb-4 box-shadow">
+                                <img class="card-img-top" src="/assets/image/Michael.jpeg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">Michael Dimakiling</h5>
+                                    <p class="card-text">I am still trying to learn programming. You can check out my about me page here.</p>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div class="btn-group">
+                                            <a href="/Michael.php" type="button" class="btn btn-sm btn-outline-secondary">View</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
